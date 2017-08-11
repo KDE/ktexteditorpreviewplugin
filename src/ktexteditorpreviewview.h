9d6bccc6913ccc5d66423d2ed51e96fc3d5ec931
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-08-12 01:54:31 +0200

    Initial import

---
+++ src/ktexteditorpreviewview.h
@@ -0,0 +1,41 @@
+/*
+ *   Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .
+ */
+
+#ifndef KTEXTEDITORPREVIEWVIEW_H
+#define KTEXTEDITORPREVIEWVIEW_H
+
+// Qt headers
+#include <QObject>
+
+namespace KTextEditor {
+class MainWindow;
+}
+
+class ktexteditorpreviewPlugin;
+
+class ktexteditorpreviewView: public QObject
+{
+    Q_OBJECT
+
+public:
+    ktexteditorpreviewView(ktexteditorpreviewPlugin* plugin, KTextEditor::MainWindow *view);
+    ~ktexteditorpreviewView() override;
+};
+
+#endif // KTEXTEDITORPREVIEWVIEW_H
